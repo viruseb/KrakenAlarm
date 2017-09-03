f74d615a8d7ce9763b841cb1b6f5b73aae71b688
Viruseb <viruseb@hotmail.com>
2017-09-03 19:29:47 +0200

    Ajout observable

--- KrekenAlarm/Hoover.py
+++ KrekenAlarm/Hoover.py
@@ -1,15 +1,26 @@
+import threading
+import time
 from threading import Thread
+from Observe import Observable
 
-class Hoover(Thread):
+class Hoover(Thread, Observable):
     """Download OHLC at continuous interval"""
 
-    def __init__(self, listpair = {}, savedir = {}, saveperiod = 43200):
+    def __init__(self):
+        Observable.__init__(self)
         Thread.__init__(self)
-        self.savedir = savedir
-        self.saveperiod = saveperiod
-        self.listpair = listpair       
-     
+        self._stop_event = threading.Event()        
+  
+    def stop(self):
+        self._stop_event.set()
+
     def run(self):
+
+        while not self._stop_event.is_set():
+            time.sleep(1);
+            for observer in self.observers:
+                observer.update(observer.pair, 16.5);
+
         return
 
 
--- KrekenAlarm/KrekenAlarm.py
+++ KrekenAlarm/KrekenAlarm.py
@@ -1,9 +1,30 @@
 
 #!/usr/bin/env python3
+import time
+from Hoover import Hoover
+from Observe import Observer
 
-import Hoover
+class StockMarket(Observer):
+    def __init__(self, pair):
+        self.pair = pair
 
-hoover = Hoover("",{"XXBTZEUR","XETHZEUR"},60*30)
+    def update(self, *args, **kwargs):
+        print("stock market received: {0}\n{1}".format(args, kwargs))
+        return
+
+eurxbt = StockMarket("XBXTZEUR")
+
+hoover = Hoover()
+hoover.register(eurxbt)
 hoover.start()
+
+print("thread start")
+time.sleep(10)
+hoover.stop()
+print("thread stopping")
+hoover.join()
+print("stopped")
+
+
 
 
